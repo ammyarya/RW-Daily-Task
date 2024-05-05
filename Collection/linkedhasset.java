0868f02b5c2fa68e26da8e5ef7eae6e84b2367a1
Amit_Arya <147738162+ammyarya@users.noreply.github.com>
2024-05-05 10:59:35 +0530

    Add files via upload

---
+++ Collection/linkedhasset.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class linkedhasset {
+
+    public static void main(String[] args) {
+
+        LinkedHashSet<String> hs = new LinkedHashSet<String>();
+
+        hs.add("Hy");
+        hs.add("Amit");
+        hs.add("Arya");
+        System.out.println("LinkedHashSet : " + hs);
+    }
+}
